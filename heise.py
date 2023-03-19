57baa0a2b0bfdad6b4ce11af3b8e00b8c14dade2
Volker Benders <vbenders@gmail.com>
2023-03-19 16:05:46 +0000

    image filename

--- heise.py
+++ heise.py
@@ -2,16 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-#browser = "firefox"
-#browser = "chrome"
-#browser = "MicrosoftEdge"
-
 browsers = ["firefox", "chrome", "MicrosoftEdge"]
 
 url = "https://www.heise.de"
-webdriver_url = "http://selenium_devcontainer-firefox-1:4444/wd/hub"
-webdriver_url = "http://webdriver:4444/wd/hub"
+
 webdriver_url = "http://selenium-hub:4444/wd/hub"
 
 
@@ -27,12 +21,17 @@ def getRemoteDriver(browser):
 
 def runTest(browser):
     driver.implicitly_wait(5)
-    #driver.maximize_window()  # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
-    driver.get(url)
-    print("Browser: {}: Title: <{}>".format(browser, driver.title))
-    #driver.save_screenshot("heise.png")
+    #driver.get(url)
+    #print("Browser: {}: Title: <{}>".format(browser, driver.title))
+    # driver.save_screenshot("heise.png")
     assert "heise" in driver.title
+    image_file(browser)
+import datetime
 
+def image_file(browser):
+    today = datetime.date.today()
+    file = "{}-{}.png".format(today, browser)
+    print("image_file: {}".format(file))
 
 if __name__ == "__main__":
     driver = None
@@ -40,10 +39,8 @@ if __name__ == "__main__":
         print("heise.py: 1")
         for browser in browsers:
             driver = getRemoteDriver(browser)
-        # driver = getProxyRemoteDriver()
-        # driver = getProxyRemoteDriver()
-        # print(f"heise.py: 2: driver: ${driver}")
-            runTest(browser)
+            image_file(browser)
+            #runTest(browser)
     finally:
         if (driver != None):
           driver.quit()
